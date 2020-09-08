8879b03ea254986b50270e1b3e7ef895dcdb47cb
i-runets <35107045+i-runets@users.noreply.github.com>
2020-09-08 20:50:38 +0000

    Deploying to gh-pages from  @ 13278b06ed6447240a008880e72158e70bf66f59 🚀

---
+++ precache-manifest.bc3856f35bfe647017595d67b7f5920d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710b76b98eacaa522ac6",
+    "url": "/css/app.6b0b3068.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9ac2ba26e6e73b111b38022f0e7a3c35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a12e57016c0b51ca339a",
+    "url": "/js/about.0b92e54c.js"
+  },
+  {
+    "revision": "710b76b98eacaa522ac6",
+    "url": "/js/app.1862d69d.js"
+  },
+  {
+    "revision": "358aa1636ce1c89e54fd",
+    "url": "/js/chunk-vendors.25891ce2.js"
+  },
+  {
+    "revision": "979e0014f78a50a1009924e89480e308",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
